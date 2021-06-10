6ac25cce425aac32ace310abf50608e9e59c7ad7
magadanthai <doctor.madbee@gmail.com>
2021-06-10 15:49:54 +0700

    Capital case change again. mistakes

--- Codemy/wsgi.py
+++ Codemy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for codemy project.
+WSGI config for Codemy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codemy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Codemy.settings')
 
 application = get_wsgi_application()
